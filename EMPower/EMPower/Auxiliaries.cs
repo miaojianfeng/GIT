b81d739842e08fdab79c37d2c1ef28d583fba2f4
MiaoJianfeng <miao.jianfeng@outlook.com>
2018-12-12 23:54:03 +0800

    Optimized UI again and adding VISA handling code

---
+++ EMPower/EMPower/Auxiliaries.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Collections.ObjectModel;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Media;
+using System.Runtime.CompilerServices;
+using System.IO;
+using System.Xml.Linq;
+
+namespace EMPower
+{
+    // ---------- UI Binding Source ----------
+    public class EMPowerParams : INotifyPropertyChanged
+    {
+        // ---------- Constructor ---------- 
+        public EMPowerParams()           
+        {
+
+        }
+
+        // ---------- Property ----------
+        private bool isComPortListExists = false;
+        private bool isConnected = false;
+        private string firmwareVer = string.Empty;
+        private string connStaMsg = "未连接";
+        private string errMsg = string.Empty;
+
+        // ---------- Property ----------
+        public bool IsComPortListExists
+        {
+            set
+            {
+                this.isComPortListExists = value;
+                NotifyPropertyChanged("IsComPortListExists");
+            }
+            get
+            {
+                return this.isComPortListExists;
+            }
+        }
+
+        public bool IsConnected
+        {
+            set
+            {
+                this.isConnected = value;
+                NotifyPropertyChanged("IsConnected");
+            }
+            get
+            {
+                return this.isConnected;
+            }
+        }
+
+        public string FirmwareVersion
+        {
+            set
+            {
+                this.firmwareVer = value;
+                NotifyPropertyChanged("FirmwareVersion");
+            }
+            get
+            {
+                return this.firmwareVer;
+            }
+        }
+
+        public string ConnectStatusMessage
+        {
+            set
+            {
+                this.connStaMsg = value;
+                NotifyPropertyChanged("ConnectStatusMessage");
+            }
+            get
+            {
+                return this.connStaMsg;
+            }
+        }
+
+        public string ErrorMessage
+        {
+            set
+            {
+                this.errMsg = value;
+                NotifyPropertyChanged("ErrorMessage");
+            }
+            get
+            {
+                return this.errMsg;
+            }
+        }
+
+        // ---------- Event ----------
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // ---------- Method ----------
+        /// <summary>
+        /// This method is called by the Set accessor of each property. 
+        /// The CallerMemberName attribute that is applied to the optional propertyName 
+        /// parameter causes the property name of the caller to be substituted as an argument.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        protected void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+
+    // ---------- Converter Class ----------
+    //public class ConnectStatusToTextConverter : IValueConverter
+    //{
+    //    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        bool isConnected = (bool)value;
+    //        if (isConnected)
+    //        {
+    //            return "已连接";
+    //        }
+    //        else
+    //        {
+    //            return "未连接";
+    //        }
+    //    }
+
+    //    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    //    {
+    //        throw new NotImplementedException("Not implement <IValueConverter.ConverBack> function");
+    //    }
+    //}
+
+    public class ConnectStateToFillColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isConnected = (bool)value;
+            LinearGradientBrush brush = new LinearGradientBrush();
+            brush.StartPoint = new Point(0, 0);
+            brush.EndPoint = new Point(1, 1);
+
+            // DarkGreen LED
+            GradientStopCollection darkGreenLED = new GradientStopCollection() { new GradientStop(Colors.DarkGreen, 0),
+                                                                                 new GradientStop(Colors.Green, 0.75),
+                                                                                 new GradientStop(Colors.LimeGreen, 0.85),
+                                                                                 new GradientStop(Colors.LightGreen, 0.9),
+                                                                                 new GradientStop(Colors.LightGreen, 1)};
+
+            // LightGreen LED
+            GradientStopCollection lightGreenLED = new GradientStopCollection() { new GradientStop(Colors.White, 0),
+                                                                                  new GradientStop(Colors.LightGreen, 0.35),
+                                                                                  new GradientStop(Colors.LimeGreen, 0.85),
+                                                                                  new GradientStop(Colors.Green, 0.9),
+                                                                                  new GradientStop(Colors.DarkGreen, 1)};
+
+            if(isConnected)
+            {
+                brush.GradientStops = new GradientStopCollection(lightGreenLED);
+            }
+            else
+            {                
+                brush.GradientStops = new GradientStopCollection(darkGreenLED);
+            }
+
+            return brush;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implement <IValueConverter.ConverBack> function");
+        }
+    }
+
+}
--- EMPower/EMPower/MainWindow.xaml.cs
+++ EMPower/EMPower/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Ivi.Visa;
+using NationalInstruments.Visa;
 
 namespace EMPower
 {
@@ -20,9 +23,229 @@ namespace EMPower
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum EnumConnectStep
+        {
+            Unknown,
+            OpenSerialPort,
+            SetGenericParams,
+            SetSerialPortParams,
+            IdnQuerying
+        }
+
+        private Dictionary<string, string> dictSerialPort = new Dictionary<string, string>();
+        private MessageBasedSession mbSession;
+        private string infoText = string.Empty;
+        private string selComPort = string.Empty;
+        static private object locker = new object();
+
+
+        private EnumConnectStep ConnectStep { set; get; }
+        private string InfoText
+        {
+            set
+            {
+                lock (locker)
+                {
+                    this.infoText = value;
+                }            
+            }
+            get
+            {
+                lock (locker)
+                {
+                    return this.infoText;
+                }
+            }
+        }
+
+        private EMPowerParams Params { set; get; }
+        private string SelectedComPort
+        {
+            set
+            {
+                lock(locker)
+                {
+                    this.selComPort = value;
+                }
+            }
+            get
+            {
+                lock(locker)
+                {
+                    return this.selComPort;
+                }
+            }
+        }
+
+        private string GetVisaAddrString(string serialPortName)
+        {
+            return this.dictSerialPort[serialPortName];
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Params = (EMPowerParams)this.FindResource("Params");
+            ConnectStep = EnumConnectStep.Unknown;
+
+            FindSerialPorts();
+        }
+
+        private void FindSerialPorts()
+        {
+            // This example uses an instance of the NationalInstruments.Visa.ResourceManager class to find resources on the system.
+            // Alternatively, static methods provided by the Ivi.Visa.ResourceManager class may be used when an application
+            // requires additional VISA .NET implementations.
+            using (var rm = new ResourceManager())
+            {
+                try
+                {
+                    IEnumerable<string> resources = rm.Find("ASRL?*INSTR");
+                    foreach (string s in resources)
+                    {
+                        ParseResult parseResult = rm.Parse(s);
+                        if(parseResult.AliasIfExists.ToUpper().Contains("COM"))
+                        {
+                            this.dictSerialPort.Add(parseResult.AliasIfExists, s);
+                            this.cboxSerialPort.Items.Add(parseResult.AliasIfExists);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    string errMsg = "没有检测到串口！\n\n" + "详细原因：\n" + ex.Message;
+                    Params.ErrorMessage = errMsg;
+                    MessageBox.Show(errMsg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private bool ConnectEMPower()
+        {
+            bool retValue = false;
+            ResourceManager rmSession = new ResourceManager();
+            string comPort = SelectedComPort;
+            string visaName = GetVisaAddrString(comPort);
+
+            try
+            {
+                //Open session
+                ConnectStep = EnumConnectStep.OpenSerialPort;
+                mbSession = (MessageBasedSession)rmSession.Open(visaName);
+
+                ConnectStep = EnumConnectStep.SetGenericParams;
+                mbSession.TerminationCharacterEnabled = true;
+                byte termChar = 0b1010;  // 0xA
+                mbSession.TerminationCharacter = termChar;
+                mbSession.TimeoutMilliseconds = 5000;
+
+                // Serial params settings
+                ConnectStep = EnumConnectStep.SetSerialPortParams;
+                ISerialSession serialSession = (ISerialSession)mbSession;
+                serialSession.BaudRate = 115200;
+                serialSession.Parity = SerialParity.None;
+                serialSession.DataBits = 8;
+                serialSession.StopBits = SerialStopBitsMode.One;
+                serialSession.FlowControl = SerialFlowControlModes.None;
+
+                // *IDN? querying 
+                ConnectStep = EnumConnectStep.IdnQuerying;
+                serialSession.RawIO.Write("*IDN?\n");
+                string resp = serialSession.RawIO.ReadString().TrimEnd("\r\n".ToCharArray());
+                if (resp.ToUpper().Contains("ETS-LINDGREN"))
+                {
+                    InfoText = resp;
+                    retValue = true;
+                }
+                else
+                {
+                    InfoText = "EMPower ID 信息错误！";
+                    retValue = false; 
+                }
+
+                return retValue;
+            }
+            catch
+            {                
+                switch (ConnectStep)
+                {
+                    case EnumConnectStep.OpenSerialPort:
+                        InfoText = string.Format("打开串口{0}失败", SelectedComPort);
+                        break;
+                    case EnumConnectStep.SetGenericParams:
+                        InfoText = "设置VISA通信端口参数失败";
+                        break;
+                    case EnumConnectStep.SetSerialPortParams:
+                        InfoText = "设置串口参数失败";
+                        break;
+                    case EnumConnectStep.IdnQuerying:
+                        InfoText = "查询EMPower ID信息失败";
+                        break;
+                    default:
+                        InfoText = "未知错误";
+                        break;
+                }
+
+                return false;                
+            }
+        }        
+
+        private async Task<bool> ConnectEMPowerAsync()
+        {
+            Task<bool> task = new Task<bool>(ConnectEMPower);
+            task.Start();
+            await task;
+            return task.Result;
+        }
+
+        private async void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {            
+            Params.IsConnected = false;
+            Params.FirmwareVersion = string.Empty;
+            Params.ConnectStatusMessage = "连接中...";
+
+            bool result = await ConnectEMPowerAsync();
+            if(result)
+            {
+                Params.IsConnected = true;
+                Params.FirmwareVersion = InfoText;
+                Params.ConnectStatusMessage = "已连接";
+            }
+            else
+            {
+                Params.IsConnected = false;
+                Params.FirmwareVersion = string.Empty;
+                Params.ConnectStatusMessage = "未连接";
+
+                string errMsg = "连接EMPower失败！\n\n" + "详细原因：\n" + InfoText;
+                MessageBox.Show(errMsg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (mbSession != null)
+            {
+                mbSession.Dispose();
+            }
+        }
+
+        private void CboxSerialPort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(Params!=null)
+            {
+                if (this.cboxSerialPort.SelectedIndex != 0)
+                {
+                    Params.IsComPortListExists = true;
+                    SelectedComPort = this.cboxSerialPort.SelectedItem.ToString();
+                }
+                else
+                {
+                    Params.IsComPortListExists = false;
+                    SelectedComPort = string.Empty;
+                }
+            }            
         }
     }
 }
